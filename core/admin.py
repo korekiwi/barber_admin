1b8c6c08058da7756af16ea34fc27b2bd30d12e9
Korekiwi <foxiknyan@gmail.com>
2025-03-06 22:56:46 +0300

    настройка фильтрации, функций подтверждения записей, поиска

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
+from django.db.models import Sum
+
 from core.models import Master, Service, Visit
+
+
+def make_accepted(modeladmin, request, queryset):
+    updated = queryset.update(status=1)
+
+
+def make_cancelled(modeladmin, request, queryset):
+    updated = queryset.update(status=2)
+
+
+def make_completed(modeladmin, request, queryset):
+    updated = queryset.update(status=3)
+
+
+def make_unverified(modeladmin, request, queryset):
+    updated = queryset.update(status=0)
+
+
+make_accepted.short_description = "Подтвердить выбранные записи"
+make_cancelled.short_description = "Отменить записи"
+make_completed.short_description = "Отметить выполненными"
+make_unverified.short_description = "Отметить неподтвержденными"
 
 
 class VisitInline(admin.TabularInline):
@@ -14,8 +38,9 @@ class VisitInline(admin.TabularInline):
 @admin.register(Master)
 class MasterAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "phone")
-    inlines = [VisitInline]
+    # inlines = [VisitInline]
     filter_horizontal = ("services",)
+    search_fields = ("first_name", "last_name")
 
 
 @admin.register(Service)
@@ -27,6 +52,20 @@ class ServiceAdmin(admin.ModelAdmin):
 
 @admin.register(Visit)
 class VisitAdmin(admin.ModelAdmin):
-    list_display = ("name", "phone", "comment", "status",)
+
+    def get_total_price(self, obj):
+        total = obj.services.aggregate(total=Sum("price"))["total"]
+        if total:
+            return f"{total:.2f} ₽"
+        return "0.00 ₽"
+
+    get_total_price.short_description = "Общая сумма"
+    get_total_price.admin_order_field = "services__price"
+
+    list_display = ("name", "phone", "comment", "status", "master", "get_total_price",)
     list_display_links = ("name",)
     list_editable = ("status",)
+    search_fields = ("phone", "name", "comment", "services__name")
+    ordering = ("master", "status", "created_at")
+    list_filter = ("master", "created_at", "status")
+    actions = [make_accepted, make_cancelled, make_completed, make_unverified]
